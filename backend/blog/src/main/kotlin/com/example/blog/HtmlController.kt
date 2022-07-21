1501b3a7435d6e6a2b87170ac9ef7a64cdd94865
kennguyen0303 <kenn.tnguyen@gmail.com>
2022-07-20 23:48:26 -0400

    First page with mustache

--- backend/blog/src/main/kotlin/com/example/blog/HtmlController.kt
+++ backend/blog/src/main/kotlin/com/example/blog/HtmlController.kt
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller class HtmlController {
     @GetMapping("/")
     fun blog(model: Model): String{
         model["title"] = "Blog"
         return "blog"
+    }
+
+    @PostMapping("/")
+    fun test(): String{
+        return "ok"
     }
 }
