e28ad47634e2973fed4d453a8bc4c0135b06e51c
kennguyen0303 <kenn.tnguyen@gmail.com>
2022-08-20 14:20:19 -0400

    Debug + commandLineRunner to populate db

---
+++ backend/demo (1)/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+
+import static java.time.Month.MARCH;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student student1 = new Student("number 1", "1@1.compareTo", LocalDate.of(1999, MARCH, 3), 33);
+            Student student2 = new Student("number 2", "1@1.compareTo", LocalDate.of(1999, MARCH, 3), 33);
+            Student student3 = new Student("number 3", "1@1.compareTo", LocalDate.of(1999, MARCH, 3), 33);
+
+            repository.saveAll(List.of(student1, student2, student3));
+        };
+
+    }
+}
